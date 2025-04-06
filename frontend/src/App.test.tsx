5578ae57c03938e4e298d91df0457644af1fbd82
anshulsingh0 <singh.1346@buckeyemail.osu.edu>
2025-04-06 18:02:11 -0400

    app test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe ('The Main App', () => {
+
+  it('Should render the Orders link', () => {
+    render(<App />);
+    expect(screen.getByText('Orders')).toBeInTheDocument();
+  });
+  it('Should render the Payments link', () => {
+    render(<App />);
+    expect(screen.getByText('Payments')).toBeInTheDocument();
+  })
+  it('Should render the Home link', () => {
+    render(<App />);
+    // expect(screen.getAllByText('Home)).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: "Home"})).toBeInTheDocument();
+  })
+  it('Should render the Home header', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', { level: 2})).toBeInTheDocument();
+  }) 
+
 });
