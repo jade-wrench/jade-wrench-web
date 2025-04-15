7aa46407bcbd88befceb943505ab73bbd76a889d
mattcopfer <21copfma@gmail.com>
2025-04-14 21:36:15 -0400

    lab 6 -updated frontend to call the api

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,10 +1,35 @@
-import data from "./data";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+type Product = {
+    id: number;
+    name: string;
+    description: string;
+    brand: string;
+    price: number;
+    rating: number;
+    numberOfReviews: number;
+    imageUrl: string;
+}
+
+const emptyProducts: Product[] = [];
 
 function Products() {
+    const [products, setProducts]: [Product[], (products: Product[]) => void] = useState(emptyProducts);
+    useEffect(() => {
+        axios.get<Product[]>("https://localhost:7250/catalog",
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            .then((response) => setProducts(response.data))
+            .catch((error) => console.log(error));
+    }, []);
     return (
         <div className="content">
             <ul className="products">
-                {data.products.map((product) => (
+                {products.map((product) => (
                     <li>
                         <div className="product">
                             <img
